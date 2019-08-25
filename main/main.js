f4088c96f0dc428366239de7d3b509b1c4c9a6ba
threey-y <threey.y@foxmail.com>
2019-08-25 15:58:33 +0800

    tasking

--- main/main.js
+++ main/main.js
@@ -1,27 +1,43 @@
 // Write your cade below:
 function caclRemaindar(dividend,divider) {
-    return dividend % divider
+    result = division(dividend,divider);
+    return result;
 };
+function division(dividend,divider){
+    let result = dividend % divider;
+    return result;
+}
 //console.log(caclRemaindar(9,4));
 
 function caclSum(arr) {
+    result = sumArr(arr);
+    return result;
+};
+
+function sumArr(arr){
     let sum = 0;
     for(let i = 0;i<arr.length;i++){
         sum += arr[i];
     }
     return sum;
-};
+}
 //console.log(caclSum([3, 6, 1, 8, 6, 4, 2]));
 
 function caclSumInConditon(arr,num) {
-    let total = 0;
+    checkArr = checkNum(arr,num);
+    result = sumArr(checkArr);
+    return result;
+};
+
+function checkNum(arr,num){
+    let checkArr = [];
     for(let i = 0;i<arr.length;i++){
         if(arr[i] < num){
-            total += arr[i];
+            checkArr.push(arr[i]);
         }
     }
-    return total;
-};
+    return checkArr;
+}
 //console.log(caclSumInConditon([3, 6, 1, 8, 6, 4, 2],5));
 
 module.exports = {
